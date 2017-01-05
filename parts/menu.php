40b7c74aa10b98a7c482e3bcddce0107dac395a7
axel <axel.bouillart@gmail.com>
2017-01-05 16:57:16 +0100

    Ajout du menu et sont contenu

---
+++ parts/menu.php
@@ -0,0 +1,30 @@
+
+<menu>
+
+<div class="mainHeader" id="partieHeader">
+		<div class="menu">
+			<div class="leftMenu">
+				<div class="ui top fixed menu">
+					<div class="item">
+						<img src="#" class="logo">
+					</div>
+					<h1 class="bordtitlemenu">Porte Folio</h1>
+					<div class="ui right item">
+						<div class="ui secondary  menu">
+							<a href="#" class="item" id="acolor">Accueil</a></li>
+							<a href="#" class="item" id="acolor">Projets</a></li>
+							<a href="#" class="item" id="acolor">Biographie</a></li>
+							<a href="#" class="item" id="acolor">Contact</a></li>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="rightMenu">
+		</div>
+
+	
+
+
+</menu>
