12b8ff20b4c5d219a60cccec97dee56e168f7ce6
axel <axel.bouillart@gmail.com>
2017-01-06 09:38:16 +0100

    Supression de div inutile

--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,23 @@
-put some header here
+<?PHP
+include "menu.php";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+
+	<header>
+
+		<div class="backgroundHeader">
+			<p>Mes projets & mes créations</p>
+		</div>
+
+	</header> 
